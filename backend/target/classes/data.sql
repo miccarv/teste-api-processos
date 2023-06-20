c67b19c2720c22a3ef72eed076633520d9a5d470
miccarv <michaelmain2020@gmail.com>
2023-06-20 14:06:50 -0300

    app properties added

---
+++ backend/target/classes/data.sql
@@ -0,0 +1,2 @@
+  INSERT INTO processos (numero, data_distribuicao, juiz, assunto, vara, area, valor, foro, partes_principais) VALUES ('0000000-00.0000.0.00.0000', '00/00/0000', 'Usuario de teste', 'Teste', 'Teste', 'Teste', 'Teste', 'teste', 'Teste');
+  INSERT INTO movimentacoes(cnjnumber_fk, data, descricao) VALUES ('0000000-00.0000.0.00.0000', '00/00/0000', 'lorem ipsum dolor sit am lorem ipsum lorem');
